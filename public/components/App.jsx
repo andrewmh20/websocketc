f475c4604fcc69853b7f841e26d9e09ff53a470a
Tzvi (Andrew) Merczynski-Hait <tzvi.meh@gmail.com>
2019-11-12 21:27:33 -0500

    Basic WS function

--- public/components/App.jsx
+++ public/components/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import WSContainer from './WSContainer';
 
 class App extends React.Component {
 
   render() {
     console.log('Hello From JSX');
     return (
-      <div><h2>Hello From React</h2></div>
+      <div><h2>Hello From React</h2>
+        <WSContainer />
+      </div>
     )
   }
 }
