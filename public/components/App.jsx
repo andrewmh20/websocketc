e516d83287f3792592468465c4e2749c8da553e7
Tzvi (Andrew) Merczynski-Hait <tzvi.meh@gmail.com>
2019-11-12 20:42:39 -0500

    Fix boilerplate

--- public/components/App.jsx
+++ public/components/App.jsx
@@ -1,1 +1,14 @@
-console.log('Hello From JSX');
+import React from 'react'
+
+class App extends React.Component {
+
+  render() {
+    console.log('Hello From JSX');
+    return (
+      <div><h2>Hello From React</h2></div>
+    )
+  }
+}
+
+
+export default App;
