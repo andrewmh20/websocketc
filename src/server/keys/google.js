1b762251ae0d2059aaa8c5da81f32fac5fe6590a
Tzvi (Andrew) Merczynski-Hait <tzvi.meh@gmail.com>
2019-12-20 14:35:40 -0500

    reset keys

--- src/server/keys/google.js
+++ src/server/keys/google.js
@@ -1,6 +1,6 @@
 
 const CLIENT_ID = '398498761232-lg0gvq0llkqmttuhbafkvk6lf6ge9gsl.apps.googleusercontent.com'
 
-const CLIENT_SECRET = '7rbyD1eEtblGlpFEZ3dC99an';
+const CLIENT_SECRET = 'TqsUt-VxKNfDVD9vlH_ZOYBG';
 
 export {CLIENT_ID, CLIENT_SECRET};
