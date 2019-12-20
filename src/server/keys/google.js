a7000dc6b0620e44d74cd99f9f8292fc50a7d7fe
Tzvi (Andrew) Merczynski-Hait <tzvi.meh@gmail.com>
2019-12-20 14:49:57 -0500

    .gitignore fix--reset api key

--- src/server/keys/google.js
+++
@@ -1,6 +0,0 @@
-
-const CLIENT_ID = '398498761232-lg0gvq0llkqmttuhbafkvk6lf6ge9gsl.apps.googleusercontent.com'
-
-const CLIENT_SECRET = 'aiUpQM0CCaPKa15IeC_3VKFS';
-
-export {CLIENT_ID, CLIENT_SECRET};
